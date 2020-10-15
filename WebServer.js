fce297ffcf7581d4ce0f6d4bf27dbd79dd4582ed
falhajri2020 <f155@live.com>
2020-10-15 21:54:11 +1100

    Done

---
+++ WebServer.js
@@ -0,0 +1,21 @@
+var express=require('express')
+var moment= require('moment')
+
+var app=express()
+
+app.use(express.static(__dirname + '/public'));
+
+
+var log=function(message){
+    var time=moment().format()
+    console.log('[Server] @'+time+' '+message)
+
+}
+var adder=function(num1,num2){
+    var result = num1+num2;
+    return result
+} 
+
+var port=3000;
+app.listen(port)
+log('Server listening on: '+port)
---
+++ public/env.js
@@ -0,0 +1,18 @@
+console.log('hello world')
+$(document).ready(function(){
+    console.log('Document ready')
+
+     $('#calculateBtn').click(function(){
+        var number1=$('#number1').val()
+        var number2=$('#number2').val()
+        var numbers={
+            num1:number1,
+            num2:number2
+        }
+        
+        $.get("http://localhost:4000/adder",numbers,function( data ){
+            console.log('Data has returned:'+ data)
+            $('#result').val(data);
+    });
+})
+})
